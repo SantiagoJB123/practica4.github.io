e9ccda34f3aa36a658927f28cf228513dba2de36
SantiagoJB123 <85642972+SantiagoJB123@users.noreply.github.com>
2022-10-03 01:42:57 -0500

    feat: "Upload files"

---
+++ js/app.js
@@ -0,0 +1,4 @@
+if(navigator.serviceWorker){
+    navigator.serviceWorker.register('https://santiagojb123.github.io/practica4.github.io/sw.js');
+    //navigator.serviceWorker.register('/sw.js');
+}
---
+++ sw.js
@@ -0,0 +1,28 @@
+self.addEventListener('install', (event) => {
+    console.log('SW: Instalado')
+    const promiseCache = caches.open('cache-v1.1')
+    .then((cache) => {
+        return cache.addAll([
+            './',
+            './index.html',
+            './css/style.css',
+            './js/app.js',
+            './pages/division.html',
+            './pages/suma.html',
+            './pages/resta.html',
+            './pages/multiplicacion.html',
+            './images/img.jpg',
+            'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css',
+            'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js'
+        ]);
+    });
+
+    event.waitUntil(promiseCache);
+});
+
+
+self.addEventListener('fetch', (event) => {
+    const respCache = caches.match(event.request);
+
+    event.respondWith(respCache);
+})
